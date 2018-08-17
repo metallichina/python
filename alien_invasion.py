a18b5da13b5db110c785abdaee4e6343fb14e7dd
metallichina <saxon-w@hotmail.com>
2018-08-17 17:19:21 +0800

    aa

--- alien_invasion.py
+++ alien_invasion.py
@@ -11,7 +11,8 @@ def run_game():
     ship = Ship(screen)
 
     while True:
-        gf.check_events()
+        gf.check_events(ship)
+        ship.update()
         gf.update_screen(ai_settings, screen, ship)
         
 
